f561192a75cf92788d4c68c27c7818c5847ef7dd
antengj <antengj@gmail.com>
2020-04-17 13:30:15 +0800

    1389. 按既定顺序创建目标数组 修改了一处语法问题

--- src/cc/techtips/leetcode/editor/cn/CreateTargetArrayInTheGivenOrder.java
+++ src/cc/techtips/leetcode/editor/cn/CreateTargetArrayInTheGivenOrder.java
@@ -71,14 +71,12 @@ public class CreateTargetArrayInTheGivenOrder {
             int size = 0;
             int[] data = new int[nums.length];
             for (int i = 0; i < nums.length; i++) {
-                if (index[i] == size) {
-                    data[index[i]] = nums[i];
-                } else {
+                if (index[i] != size) {
                     for (int j = size; j > index[i]; j--) {
                         data[j] = data[j - 1];
                     }
-                    data[index[i]] = nums[i];
                 }
+                data[index[i]] = nums[i];
                 size++;
             }
             return data;
