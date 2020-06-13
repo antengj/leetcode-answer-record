497f950946c6719a4e32afc1714ab5bf1e2d4f97
antengj <antengj@gmail.com>
2020-06-13 16:16:13 +0800

    70. 爬楼梯

---
+++ src/cc/techtips/leetcode/editor/cn/ClimbingStairs.java
@@ -0,0 +1,54 @@
+/**
+ * @title 70. 爬楼梯
+ * @link https://leetcode-cn.com/problems/climbing-stairs/
+ * @description
+ *      假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ *      每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ *      注意：给定 n 是一个正整数。
+ *
+ *      示例 1： 
+ *      输入： 2
+ *      输出： 2
+ *      解释： 有两种方法可以爬到楼顶。
+ *      1.  1 阶 + 1 阶
+ *      2.  2 阶
+ *
+ *      示例 2： 
+ *      输入： 3
+ *      输出： 3
+ *      解释： 有三种方法可以爬到楼顶。
+ *      1.  1 阶 + 1 阶 + 1 阶
+ *      2.  1 阶 + 2 阶
+ *      3.  2 阶 + 1 阶
+ * @tags 动态规划
+ */
+package cc.techtips.leetcode.editor.cn;
+
+public class ClimbingStairs {
+    public static void main(String[] args) {
+        Solution solution = new ClimbingStairs().new Solution();
+        System.out.println(solution.climbStairs(1));
+        System.out.println(solution.climbStairs(2));
+        System.out.println(solution.climbStairs(3));
+        System.out.println(solution.climbStairs(4));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int climbStairs(int n) {
+
+            if (n == 0 || n == 1) {
+                return 1;
+            }
+            int[] steps = new int[n + 1];
+            steps[1] = 1;
+            steps[2] = 2;
+            for (int i = 3; i <= n; i++) {
+                steps[i] = steps[i - 1] + steps[i - 2];
+            }
+            return steps[n];
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+}
