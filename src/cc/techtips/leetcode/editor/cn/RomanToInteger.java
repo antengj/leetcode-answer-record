42cc005f90b6f3c7df065c4199380a4199686700
antengj <antengj@gmail.com>
2021-07-02 15:37:35 +0800

    13. 罗马数字转整数

---
+++ src/cc/techtips/leetcode/editor/cn/RomanToInteger.java
@@ -0,0 +1,24 @@
+package cc.techtips.leetcode.editor.cn;
+
+/**
+ * @title 13. 罗马数字转整数
+ * @link https://leetcode-cn.com/problems/roman-to-integer/
+ * @descriptioon
+ * @tags 哈希表 数学 字符串
+ */
+public class RomanToInteger {
+    public static void main(String[] args) {
+        RomanToInteger.Solution solution = new RomanToInteger().new Solution();
+        System.out.println(solution.romanToInt("a"));
+        System.out.println(solution.romanToInt("b"));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int romanToInt(String s) {
+            return -1;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+}
