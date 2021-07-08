6a48871d7d10cf898ec301d30fa89df520e698d2
antengj <antengj@gmail.com>
2021-07-08 09:33:31 +0800

    14. 最长公共前缀

---
+++ src/cc/techtips/leetcode/editor/cn/LongestCommonPrefix.java
@@ -0,0 +1,40 @@
+package cc.techtips.leetcode.editor.cn;
+
+/**
+ * @title 14. 最长公共前缀
+ * @link https://leetcode-cn.com/problems/longest-common-prefix/
+ * @description
+ *      编写一个函数来查找字符串数组中的最长公共前缀。
+ *      如果不存在公共前缀，返回空字符串 ""。
+ *
+ *      示例 1：
+ *      输入：strs = ["flower","flow","flight"]
+ *      输出："fl"
+ *
+ *      示例 2：
+ *      输入：strs = ["dog","racecar","car"]
+ *      输出：""
+ *      解释：输入不存在公共前缀。
+ *
+ *      提示：
+ *      0 <= strs.length <= 200
+ *      0 <= strs[i].length <= 200
+ *      strs[i] 仅由小写英文字母组成
+ * @tags 字符串
+ */
+public class LongestCommonPrefix {
+    public static void main(String[] args) {
+        Solution solution = new LongestCommonPrefix().new Solution();
+        System.out.println(solution.longestCommonPrefix(new String[]{}));
+    }
+    
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public String longestCommonPrefix(String[] strs) {
+            return null;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+}
+
